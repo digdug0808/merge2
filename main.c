e7e2f5c22d0736d358f0b38502c8e9863d1cf043
catetcpasswd <randy.gonzales@gmail.com>
2013-08-14 10:56:53 +0800

    main.c

--- main.c
+++ main.c
@@ -5,10 +5,15 @@ main() {
 	printf("Hello world\n");
 	branch();
 	hotfix();
+	newfn();
 }
 
 int hotfix() {
 	int i;
 	i = 2;
 	printf("Hotfix #: %d fixed\n", i);
+}
+
+int newfun() {
+	printf("its more fun in the philippines\n");
 }
